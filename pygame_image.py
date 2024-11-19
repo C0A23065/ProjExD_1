f7475326d08fadc8ffb46592fc9e86a8848d1f25
Takumi <c0a2306550@edu.teu.ac.jp>
2024-11-19 15:32:58 +0900

    practice 2

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -19,6 +21,7 @@ def main():
         pg.display.update()
         tmr += 1        
         clock.tick(10)
+        
 
 
 if __name__ == "__main__":
