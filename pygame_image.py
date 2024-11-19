e8caa370102791aae7dd90877291ed41d65a9b70
Takumi <c0a2306550@edu.teu.ac.jp>
2024-11-19 16:38:25 +0900

    practice 8

--- pygame_image.py
+++ pygame_image.py
@@ -13,18 +13,32 @@ def main():
     bg_img_f = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    screen.blit(kk_img, kk_rct)
+    
     tmr = 0
     bg_x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
         x = -(tmr % 3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img_f, [x + 1600, 0])
         screen.blit(bg_img, [x + 3200, 0])
         screen.blit(bg_img_f, [x + 4800, 0])
-        screen.blit(kk_img,[300, 200])
+        #screen.blit(kk_img,[300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
         tmr += 1
         
             
