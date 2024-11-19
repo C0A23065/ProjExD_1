ce0cff73b9f96dddf42b16dafd9d4e0a205e2a5d
Takumi <c0a2306550@edu.teu.ac.jp>
2024-11-19 16:59:36 +0900

    ex1

--- pygame_image.py
+++ pygame_image.py
@@ -31,14 +31,16 @@ def main():
         #screen.blit(kk_img,[300, 200])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
+        kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            kk_rct.move_ip(0,-1)
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            kk_rct.move_ip(0, +1)
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
+            kk_rct.move_ip(+2, 0)
+        
         tmr += 1
         
             
